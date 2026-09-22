b346fd27ede4cd60af66f0acdcd6eadd89e28ad3
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-22 14:00:19 +0600

    Add Business Overview screen

---
+++ app/src/main/java/com/tamanna/admin/BusinessOverviewActivity.kt
@@ -0,0 +1,22 @@
+package com.tamanna.admin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class BusinessOverviewActivity : AppCompatActivity() {
+
+    private lateinit var tvStatus: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business_overview)
+
+        tvStatus = findViewById(R.id.tvOverviewStatus)
+
+        findViewById<Button>(R.id.btnRefreshOverview).setOnClickListener {
+            tvStatus.text = "Overview refreshed"
+        }
+    }
+}
